e8d6d65a8070184537e3ccd5efb2600d869231f3
Pavan-kumar-vm <153340194+Pavan-kumar-vm@users.noreply.github.com>
2026-05-15 00:18:27 +0530

    Update backend.tf

--- terra-config/backend.tf
+++ terra-config/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pavan262-terra-state-bucket"  # Change if the name already exists. 
+    bucket         = "pavan264-terra-state-bucket"  # Change if the name already exists. 
     key            = "eks/terraform.tfstate"       
     region         = "us-east-1"                   
     encrypt        = true
